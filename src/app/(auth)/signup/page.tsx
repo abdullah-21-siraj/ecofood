18c7246b947223734732110dbd7222ffd0746709
Taha Sheikh <tahashayk2k@gmail.com>
2024-04-28 00:23:23 +0500

    signup implementation frontend with backend, navbar set and styled

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,55 +1,204 @@
+"use client"
+import Link from "next/link";
 import React from "react";
+import { useState } from "react";
 
 const SignupPage = () => {
+  const [gender, setGender] = useState("")
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dob, setDob] = useState("");
+
+  async function onClickSubmit()
+  {
+    try {
+      const response = await fetch("/api/authenticate/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+    
+        body: JSON.stringify({
+          gender:gender,
+          fname:firstName,
+          lname:lastName,
+          email:email,
+          password:password,
+          dob:dob
+        }),
+      })
+
+      if(!response.ok)
+        {
+          alert("Error submitting data");
+          return;
+        }
+
+        alert("Account created successfully");
+        setEmail("");
+        setPassword("");
+        setGender("");
+        setFirstName("");
+        setLastName("");
+        setDob("");
+
+    } catch (error) {
+      
+    }
+  }
+
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex w-full  bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Create an account
+        <div className="flex flex-col space-y-6">
+          <h2 className="mt-6 text-left text-3xl font-extrabold text-gray-700">
+            CREATE AN ACCOUNT
           </h2>
+          <span className="text-gray-500">Already have an Account? <Link className="text-black" href="/login">Login in instead!</Link></span>
         </div>
-        <form className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm -space-y-px">
+        <div className="mt-8 space-y-6 w-full">
+          <div className="flex flex-col w-full space-y-8">
+
+            <div className="flex flex-row justify-between w-full">
+            <label htmlFor="email-address" >
+                  Social title
+                </label>
+
+
+              <div className="flex flex-row space-x-4">  
+              <div className="flex flex-row space-x-3">
+                <input
+                  id="Mr"
+                  name="gender"
+                  type="radio"
+
+                  value={gender}
+                  onClick={() => setGender("Mr")}
+                />
+                <span>Mr</span>
+              </div>
+
+
+              <div className="flex flex-row space-x-3">
+
+                <input
+                  id="Mrs"
+                  name="gender"
+                  type="radio"
+                  value={gender}
+                  onClick={() => setGender("Mrs")}
+                />
+                <span>Mrs</span>
+              </div>
+
+
+              </div>
+
+
+            </div>
+
+
+            <div className="flex flex-row justify-between w-full">
+              <span className="w-full">
+                First Name
+              </span>
+
+              <input
+                id="fname"
+                name="fname"
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="First Name..."
+              />
+            </div>
+
+
+            <div className="flex flex-row justify-between w-full">
+              <span className="w-full">
+                Last Name
+              </span>
+
+              <input
+                id="lname"
+                name="lname"
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Last Name..."
+              />
+            </div>
+
+
+
             <div>
-              <label htmlFor="email-address" className="sr-only">
+              <label htmlFor="email-address" className="">
                 Email address
               </label>
               <input
                 id="email-address"
                 name="email"
                 type="email"
-                autoComplete="email"
-                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
               />
             </div>
+
+
+            
+
             <div>
-              <label htmlFor="password" className="sr-only">
+              <label htmlFor="password" >
                 Password
               </label>
               <input
                 id="password"
                 name="password"
                 type="password"
-                autoComplete="new-password"
-                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
             </div>
+
+
+
+            <div className="flex flex-row justify-between w-full">
+              <span className="w-full">
+                Date of Birth
+              </span>
+
+              <input
+                id="dob"
+                name="dob"
+                type="date"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              />
+            </div>
+
+
+
           </div>
 
           <div>
             <button
-              type="submit"
+            onClick={onClickSubmit}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Sign up
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/app/homePageComponents/navbar/page.tsx
+++ src/app/homePageComponents/navbar/page.tsx
@@ -3,20 +3,44 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import SearchComponent from "@/app/searchComponents";
+// import SearchComponent from "@/app/searchComponents";
+import { Search } from 'lucide-react';
+import { Repeat2 } from 'lucide-react';
+import { Heart } from "lucide-react";
+import { User } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [showSubTypes, setShowSubTypes] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+
+  const searchProducts = [{ id: 1, name: "mango", price: "$23.12" }, { id: 2, name: "Apple", price: "$11.12" }, { id: 3, name: "Orange", price: "$50.12" }, { id: 4, name: "Banana", price: "$26.07" }, { id: 5, name: "Grapes", price: "$21.56" }, { id: 6, name: "Guava", price: "$12.53" }]
+
+  const handleInputChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // function searchProductOnChange(search:any)
+  // {
+  //  const searchedProducts = searchProducts?.includes(search);
+  // }
+
+  const searchedProducts = searchTerm
+    ? searchProducts.filter((product: any) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    : [];
+
   return (
     <>
       <header className="bg-contrast-green-500 text-black bg-zinc-100">
-        <div className="flex flex-row flex-grow lg:flex bg-emerald-600 text-white">
+        <div className="flex flex-row flex-grow lg:flex bg-[#458E7A] text-white">
           <Link
             href="/pages/contactUs"
             className="p-2 bg-contrast-green-700 flex-grow hover:text-white transition duration-300 hidden lg:block md:block text-center"
@@ -26,7 +50,7 @@ export default function NavBar() {
           </Link>
         </div>
 
-        <nav className="flex flex-wrap lg:flex-row p-2 justify-between items-center bg-green-100">
+        <nav className="flex flex-wrap lg:flex-row p-2 justify-between items-center bg-[#F2F2F2]">
           <div className="text-center m-3 font-bold">
             <Link href="/">
               <Image
@@ -39,27 +63,54 @@ export default function NavBar() {
             </Link>
           </div>
           <div className="flex flex-row items-center justify-center flex-grow lg:flex">
-            <div className="text-center m-3 hover:text-white transition duration-300 hidden lg:block md:block">
-              My Compare
+
+            <div className="flex flex-row justify-between items-center">
+              <div className="text-center m-3  items-center cursor-pointer transition space-x-3 font-semibold duration-300 hidden lg:flex md:flex">
+                <Repeat2 size={20} />
+                <span>My Compare</span>
+              </div>
+              <span className="mx-3 text-[#CECECE]">|</span>
+
+              <div className="text-center m-3  space-x-3 cursor-pointer items-center transition font-semibold duration-300 hidden lg:flex md:flex">
+                <Heart size={20} />
+                <span>My Favourites</span>
+              </div>
             </div>
-            <div className="text-center m-3 hover:text-white transition duration-300 hidden lg:block md:block">
-              My Favourites
+
+            
+            <div className="text-center relative m-3 w-[40%] bg-white flex justify-between flex-row px-8 py-3 rounded-full transition duration-300  lg:flex md:flex">
+              <input className="bg-transparent outline-none focus:outline-none" placeholder="Search..." type="search" name="" id="" value={searchTerm}
+                onChange={handleInputChange} />
+              <Search color="#458E7A" size={20}/>
+            <div className={`absolute ${searchedProducts.length > 0 ? 'block' : 'hidden'} cursor-pointer z-20 mt-12 left-2  w-[400px] shadow-md  h-[600px] bg-white max-h-[600px] overflow-y-auto`}>
+              
+              <div className="flex flex-col space-y-3">
+              {searchedProducts.map((product: any) => (<div className=" space-x-2 py-4 px-2 flex items-center text-black ">
+                <Image alt="/productImage" src={"/randomPhoto.jpg"} width={100} height={100}/>
+                
+                <div className="flex flex-col justify-start space-y-6 ">
+                <h1 className="text-xl font-semibold">{product.name}</h1>
+                <span className="text-xs text-gray-600">{product.price}</span>
+                </div>
+                
+              </div>
+              ))}
+              </div>
             </div>
-            <div className="text-center m-3 hover:text-white transition duration-300 hidden lg:block md:block">
-              Search
             </div>
+            
+
+
           </div>
 
-<<<<<<< Updated upstream
-          <div className="ml-auto flex flex-row items-center justify-center lg:justify-end hidden lg:block md:block ">
-=======
-          <div className="ml-auto flex flex-row items-center justify-center lg:justify-end  lg:block md:block">
->>>>>>> Stashed changes
-            <div className="text-center m-3 hover:text-white transition duration-300">
-              My Account
+          <div className="ml-auto flex flex-row items-center justify-center lg:justify-end  lg:block md:block ">
+            <div className="text-center m-3 cursor-pointer flex space-x-3 transition duration-300">
+              <User />
+              <span className="font-semibold"> My Account </span>
             </div>
-            <div className="text-center m-3 hover:text-white transition duration-300">
-              Add to Cart
+            <div className="text-center m-3 cursor-pointer flex space-x-3 transition duration-300">
+              <ShoppingCart />
+              <span className="font-semibold">Add to Cart</span>
             </div>
           </div>
           <div className="ml-auto flex flex-row items-center justify-center lg:justify-end">
@@ -177,9 +228,8 @@ export default function NavBar() {
           </Link>
         </div>
         <div
-          className={`${
-            menuOpen ? "flex flex-col" : "hidden"
-          } lg:hidden md:hidden`}
+          className={`${menuOpen ? "flex flex-col" : "hidden"
+            } lg:hidden md:hidden`}
         >
           <Link
             href="/"
